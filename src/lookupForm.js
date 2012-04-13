ce48d38c7e33fbcf3dbba4e17504effbacb50d83
Darren Lewis <lewisd35@gmail.com>
2012-04-13 21:57:29 +0100

    Renamed main directory

---
+++ src/lookupForm.js
@@ -0,0 +1,35 @@
+
+//LATEST VERSION OF THE CODE IS AVAILABLE AT http://github.com/Daxaar
+
+function ViewModel(){
+
+	this.title = "This is the title of the page";
+	this.description = "Here is the description";
+    //Add as many columns as you like to the headings array
+	this.headings = ["Full Name", "Phone", "Business", "Site", "Title", "Email Address"];
+    //Fixed width for now.  I'll hook something up that sets based on length of content
+    this.columnWidth = 150;
+	//Array of data.  Make sure the number of elements in the array matches the heading count
+    this.entities = [
+                    ["Darren Lewis", "000-111-2222","Octono Limited", "Birmingham", "Developer", "darren@octono.com"],
+		            ["Noriyuki Haga", "333-444-7777", "Swan Yamaha", "Up North", "Motorcycle Rider", "nori@here.com"]
+		           ];
+    //If you provide a row icon set this to a relative url.  Otherwise just leave blank and the image will be
+    //omitted from the row (visibility: none)
+    this.rowIcon = "",
+    //actions for each of the buttons on the form
+    this.actions = { 
+                    ok: function () { alert('ok'); }, 
+                    cancel: function () { alert('cancel'); },
+                    create: function () { alert('create'); },
+                    properties: function () { alert('properties'); },
+                    remove: function () { alert('remove'); },
+                    selected: function (row){alert(row)}
+                };
+}
+
+$(function () {
+
+    ko.applyBindings(new ViewModel());
+
+});
